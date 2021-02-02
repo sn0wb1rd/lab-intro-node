92b5d5dcd3ccba34bee07fe7d5aa1760c0f92ecc
Cindy <cindytvn+git@gmail.com>
2021-02-02 20:12:10 +0100

    Delia + Cindy: lab solved, incl bonus

--- index.js
+++ index.js
@@ -1,17 +1,77 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a, b) {
+      return a - b
+    })
+  
+    this.length = this.items.length
+    return this.items
+    }
 
-  get(pos) {}
+  get(pos) {
+    if (!(this.items.indexOf(pos) === -1)){
+      return this.items.find(elem => elem == pos)      
+    }
+    else {
+      throw new Error('OutOfBounds');
+    }
+  };
 
-  max() {}
+  max() {
+    if (this.length != 0){
+      return Math.max(...this.items)
+    }
+    else {
+      throw new Error('EmptySortedList');
+    }
+  };
 
-  min() {}
+  min() {
+    
+    if (this.length != 0){
+      return Math.min(...this.items)
+    }
+    else {
+      throw new Error('EmptySortedList');
+    }
+  };
 
-  sum() {}
+  sum() {
+    if (this.length != 0){
+       return this.items.reduce(function (accumulator, currentValue){
+        accumulator += currentValue
+        return accumulator;
+      }, 0);
+    }
+    else {
+      return 0
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length != 0){
+      // return sum() / this.length // does not work?!
+      let sumNbr = this.items.reduce(function (accumulator, currentValue){
+        accumulator += currentValue
+        return accumulator;
+      }, 0);
+      return sumNbr / this.length
+
+
+    }
+    else {
+      throw new Error('EmptySortedList');
+    }
+    
+
+
+  }
 }
 
 module.exports = SortedList;
